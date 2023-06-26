830712610333d7985e48daf132f5582a9fe6056b
karanvirsingh011998 <karanvir011998@gmail.com>
2023-06-26 15:47:41 +0530

    added lib for google login npm install react-google-login

---
+++ src/login.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+
+const clientId =
+  "737293521504-td52m9hjqmnm500aavametlake3lb8ok.apps.googleusercontent.com";
+
+function Login() {
+  const [showloginButton, setShowloginButton] = useState(true);
+  const [showlogoutButton, setShowlogoutButton] = useState(false);
+
+  const onLoginSuccess = (res) => {
+    console.log("Login Success:", res.profileObj);
+    setShowloginButton(false);
+    setShowlogoutButton(true);
+
+    // const popup = window.open("", "_self");
+    // popup.close();
+  };
+
+  const onLoginFailure = (res) => {
+    console.log("Login Failed:", res);
+  };
+
+  const onSignoutSuccess = () => {
+    alert("You have been logged out successfully");
+    console.clear();
+    setShowloginButton(true);
+    setShowlogoutButton(false);
+  };
+
+  return (
+    <div>
+      {showloginButton ? (
+        <GoogleLogin
+          clientId={clientId}
+          buttonText="Sign In"
+          onSuccess={onLoginSuccess}
+          onFailure={onLoginFailure}
+          cookiePolicy={"single_host_origin"}
+          isSignedIn={true}
+          uxMode="popup" // Add this line
+        />
+      ) : null}
+
+      {showlogoutButton ? (
+        <GoogleLogout
+          clientId={clientId}
+          buttonText="Sign Out"
+          onLogoutSuccess={onSignoutSuccess}
+        ></GoogleLogout>
+      ) : null}
+    </div>
+  );
+}
+
+export default Login;
